dfa577f5eb68bb2877c1cf68909248aca27faea2
Kavindu Ravishan <ravishankavindu@gmail.com>
2024-03-25 19:13:10 +0200

    login feature added

--- client/src/auth/AuthWrapper.js
+++ client/src/auth/AuthWrapper.js
@@ -1,48 +1,44 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
 import { RenderHeader } from "../components/structure/Header";
 import { RenderMenu, RenderRoutes } from "../components/structure/RenderNavigation";
 
 const AuthContext = createContext();
 export const AuthData = () => useContext(AuthContext);
 
-
 export const AuthWrapper = () => {
+  const [user, setUser] = useState({ name: "", isAuthenticated: false });
 
-     const [ user, setUser ] = useState({name: "", isAuthenticated: false})
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post("http://localhost:8000/api/login", { email, password });
 
-     const login = (userName, password) => {
+      if (response.status === 200) {
+        setUser({ name: email, isAuthenticated: true });
+        return "success"; // Return success if login is successful
+      } else if (response.status === 401) {
+        throw new Error("Incorrect password"); // Throw error if password is incorrect
+      } else if (response.status === 404) {
+        throw new Error("User not found"); // Throw error if user is not found
+      } else {
+        throw new Error("Unexpected error occurred"); // Throw error for other unexpected errors
+      }
+    } catch (error) {
+      throw error.response?.data?.error || "An error occurred";
+    }
+  };
 
-          // Make a call to the authentication API to check the username
-          
-          return new Promise((resolve, reject) => {
+  const logout = () => {
+    setUser({ ...user, isAuthenticated: false });
+  };
 
-               if (password === "password") {
-                    setUser({name: userName, isAuthenticated: true})
-                    resolve("success")
-               } else {
-                    reject("Incorrect password")
-               }
-          })
-          
-          
-     }
-     const logout = () => {
-
-          setUser({...user, isAuthenticated: false})
-     }
-
-
-     return (
-          
-               <AuthContext.Provider value={{user, login, logout}}>
-                    <>
-                         <RenderHeader />
-                         <RenderMenu />
-                         <RenderRoutes />
-                    </>
-                    
-               </AuthContext.Provider>
-          
-     )
-
-}
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      <>
+        <RenderHeader />
+        <RenderMenu />
+        <RenderRoutes />
+      </>
+    </AuthContext.Provider>
+  );
+};
