c68ccec99e14bd06c3551cd4f1fe84e60821eabb
Kavindu Ravishan <ravishankavindu@gmail.com>
2024-03-25 13:53:40 +0200

    code cleaning

--- server/models/url.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const urlSchema = new mongoose.Schema({
-  originalUrl: String,
-  shortUrl: String,
-  clickCount: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model('URL', urlSchema);
